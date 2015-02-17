76c1c7cace33de6816d79bea21339c32529a5a40
LuHoDIj <lozan.vasya@mail.ru>
2015-02-17 16:08:50 +0200

    test4

--- src/Main/MainBundle/Controller/IndexController.php
+++ src/Main/MainBundle/Controller/IndexController.php
@@ -3,11 +3,23 @@
 namespace Main\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class IndexController extends Controller
 {
     public function indexAction()
     {
         return $this->render('MainMainBundle:Default:index.html.twig');
+    }
+
+    public function registerAction($sponsor_id)
+    {
+        $user = $this->getDoctrine()->getRepository('MainMainBundle:User')->find($sponsor_id);
+        $em = $this->getDoctrine()->getManager();
+        if ($user != null)
+        {
+             User::addUser($em, $user);
+            return $this->render('MainMainBundle:Default:register.html.twig', array('user' =>$user));
+        }
     }
 }
