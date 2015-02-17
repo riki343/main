dfb02643d38fe9a9fade6588399a029779ff8e9e
LuHoDIj <lozan.vasya@mail.ru>
2015-02-17 17:05:42 +0200

    maping

--- src/Main/MainBundle/Controller/IndexController.php
+++ src/Main/MainBundle/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace Main\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class IndexController extends Controller
@@ -12,8 +13,25 @@ class IndexController extends Controller
         return $this->render('MainMainBundle:Default:index.html.twig');
     }
 
-    public function registerAction($sponsor_id)
+    public function registerAction(Request $request, $sponsor_id)
     {
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        $repeatPassword = $request->request->get('repeatPassword');
+        $email = $request->request->get('email');
+        $name = $request->request->get('name');
+        $surname = $request->request->get('surname');
+        $perfectMoney = $request->request->get('perfectMoney');
+        $statusAgreeRules = $request->request->get('statusAgreeRules');
+        $statusAgreeEthics = $request->request->get('statusAgreeEthics');
+
+        $parameters = array(
+            'username' => $username,
+            'password' => $password,
+            'repeatPassword' => $repeatPassword,
+
+        );
+
         $user = $this->getDoctrine()->getRepository('MainMainBundle:User')->find($sponsor_id);
         $em = $this->getDoctrine()->getManager();
         if ($user != null)
