db5618dcec65d5e23440684c211d984186f67781
riki343 <riki34@spaces.ru>
2015-02-17 15:55:42 +0200

    Entity

---
+++ src/Main/MainBundle/Entity/User.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Main\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="users")
+ */
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="surname", type="string", length=255)
+     */
+    private $surname;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="registered", type="datetime")
+     */
+    private $registered;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="lastactive", type="datetime")
+     */
+    private $lastactive;
+
+    /**
+     * @var boolean
+     * @ORM\Column(name="active", type="boolean")
+     */
+    private $active;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="perfect_money", type="string", length=8)
+     */
+    private $perfectMoney;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="avatar", type="string", length=255)
+     */
+    private $avatar;
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @inheritDoc
+     * @return multitype:string
+     */
+    public function getRoles()
+    {
+        return $this->roles->toArray();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSalt()
+    {
+        return '7v8b6ghjb6834bdkjndsjb233409fjvsiu8892d';
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * @see \Serializable::serialize()
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->password,
+            $this->email,
+            $this->name,
+            $this->surname,
+            $this->registered,
+            $this->lastactive,
+            $this->perfectMoney,
+            $this->active
+        ));
+    }
+
+    /**
+     * @see \Serializable::unserialize()
+     */
+    public function unserialize($serialized)
+    {
+        list(
+            $this->id,
+            $this->password,
+            $this->email,
+            $this->name,
+            $this->surname,
+            $this->registered,
+            $this->lastactive,
+            $this->perfectMoney,
+            $this->active) = unserialize($serialized);
+    }
+}
