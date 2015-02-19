36cd3694539028a77ed39b5c0190b26bddf69a32
LuHoDIj <lozan.vasya@mail.ru>
2015-02-19 14:11:21 +0200

    sadcfvgnm

--- src/Main/MainBundle/Controller/UserController.php
+++ src/Main/MainBundle/Controller/UserController.php
@@ -9,8 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Main\MainBundle\Extras\ChromePhp as console;
 
 class UserController extends Controller {
+
     public function indexAction(Request $request) {
         $user = $this->getUser();
         return $this->render('MainMainBundle::userpage.html.twig');
+    }
+
+    public function statisticsAction(Request $request)
+    {
+        return $this->render('MainMainBundle::statistics.html.twig');
     }
 }
