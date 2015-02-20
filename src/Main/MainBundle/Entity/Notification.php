5f304c9520895073ef5126cecde0e5aa10de1bed
LuHoDIj <lozan.vasya@mail.ru>
2015-02-20 19:34:54 +0200

    notifications

---
+++ src/Main/MainBundle/Entity/Notification.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Main\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Notification
+ *
+ * @ORM\Table(name="notifications")
+ * @ORM\Entity
+ */
+class Notification
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="userid", type="integer")
+     */
+    private $userid;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    private $message;
+
+    /**
+     * @var boolean
+     * @ORM\Column(name="active", type="boolean")
+     */
+    private $active;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="registered", type="datetime")
+     */
+    private $registered;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserid()
+    {
+        return $this->userid;
+    }
+
+    /**
+     * @param int $userid
+     */
+    public function setUserid($userid)
+    {
+        $this->userid = $userid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param boolean $active
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getRegistered()
+    {
+        return $this->registered;
+    }
+
+    /**
+     * @param \DateTime $registered
+     */
+    public function setRegistered($registered)
+    {
+        $this->registered = $registered;
+    }
+
+    public function getInArray()
+    {
+        return array(
+            'id' => $this->getId(),
+            'user' => $this->getUser()->getInArray(),
+            'message' => $this->getMessage(),
+            'activate' => $this->getActivated(),
+            'registered' => $this->getRegistered()
+        );
+    }
+}
--- src/Main/MainBundle/Services/Notifier.php
+++ src/Main/MainBundle/Services/Notifier.php
@@ -5,6 +5,7 @@ namespace Main\MainBundle\Services;
 use Doctrine\ORM\Mapping as ORM;
 //use Main\MainBundle\Entity\Notification;
 //use Main\MainBundle\Entity\Message;
+use Intranet\MainBundle\Entity\Notification;
 use Main\MainBundle\Entity\User;
 
 class Notifier {
@@ -12,4 +13,49 @@ class Notifier {
     private $em = null;
     private $router = null;
     private $mailer = null;
+
+    public function __construct($securityContext, $em, $router, $mailer)
+    {
+        $this->user = $securityContext->getToken()->getUser();
+        $this->em = $em;
+        $this->router = $router;
+        $this->mailer = $mailer;
+    }
+
+    public function sendNotificationToEmail($user, $message)
+    {
+        try {
+            $message = \Swift_Message::newInstance()
+                ->setSubject('notification!')
+                ->setFrom('support@main.com')
+                ->setTo($user->getEmail())
+                ->setBody($message, 'text/html');
+            $this->mailer->send($message);
+        } catch (\Swift_RfcComplianceException $ex) {  }
+    }
+
+    public function createNotification($newUser)
+    {
+        $message = "На ваш счет зачислено 3$ от: " . $newUser->getName() . " " . $newUser->getSurname . "который пресоединился к вашей команде!"
+        $notification = new Notification();
+        $notification->setUserid($this->user->getId());
+        $notification->setMessage($message);
+        $notification->setActive(false);
+        $notification->setRegistered(new \DateTime());
+        $this->em->persist($notification);
+		$this->em->flush();
+
+        $this->sendNotificationToEmail($newUser, $message);
+    }
+
+    public function clearNotifications()
+    {
+        $notifications = $this->em->getRepository('MainMainBundle:Notification')->findBy(array('activate' => true))
+        if ($notifications == null) return;
+        foreach($notifications as $notification)
+        {
+            $this->em->remove($notification)
+        }
+        $this->em->flush();
+    }
 }
